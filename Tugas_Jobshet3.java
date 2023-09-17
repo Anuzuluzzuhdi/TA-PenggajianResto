42e2f0b0396fafd5fcf079efe3e2d3b4687e4b87
EksaMuhammad <144184459+EksaMuhammad@users.noreply.github.com>
2023-09-17 23:21:07 +0700

    update: input gaji

--- Tugas_Jobshet3.java
+++ Tugas_Jobshet3.java
@@ -2,7 +2,15 @@ import java.util.Scanner;
 public class Tugas_Jobshet3 {
     public static void main(String[] args) {
 
-        
+        Scanner sc = new Scanner(System.in);
+       
+       int gajipokok, bonus, sanksi; 
+       float slipgaji;
+
+       System.out.println("Masukkan gaji pokok: " );
+       gajipokok = sc.nextInt();
+       System.out.println("Masukkan berapa hari alpha: ");
+       sanksi = sc.nextInt();
     }
     
 }
